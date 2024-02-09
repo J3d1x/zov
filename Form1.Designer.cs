4878c4435665b3e877d4c8eca8e267645f487ceb
J3d1x <okulovroman@bk.ru>
2024-02-09 22:59:46 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace _123
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            ""});
+            this.listBox1.Location = new System.Drawing.Point(136, 282);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(167, 108);
+            this.listBox1.TabIndex = 22;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(379, 302);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(265, 23);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Формирование квитанции";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(133, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Длина";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Ширина";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(556, 96);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(88, 17);
+            this.checkBox2.TabIndex = 18;
+            this.checkBox2.Text = "Фотопечать";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(556, 71);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(113, 17);
+            this.checkBox1.TabIndex = 17;
+            this.checkBox1.Text = "Многоуровневый";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(203, 182);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 16;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(203, 140);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 15;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(131, 95);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(71, 17);
+            this.radioButton2.TabIndex = 14;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Матовые";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(131, 61);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(81, 17);
+            this.radioButton1.TabIndex = 13;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Глянцевые";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(137, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace _123
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public double result;
+        public string lvl;
+        public string photo;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            try
+            {
+                double height = Convert.ToDouble(textBox1.Text);
+                double weight = Convert.ToDouble(textBox2.Text);
+
+                if (textBox1.Text == "" || textBox2.Text.Length == 0)
+                {
+                    MessageBox.Show("zapolnite polya!");
+                    return;
+                }
+
+                if (radioButton1.Checked == true)
+                {
+                    result = height * weight * 213.15;
+                }
+                else if (radioButton2.Checked == true)
+                {
+                    result = height * weight * 265.80;
+                }
+
+                if (checkBox1.Checked == true)
+                {
+                    lvl = "Выбрано";
+                    result *= 1.3;
+                }
+                else
+                {
+                    lvl = "Не выбрано";
+                }
+
+                if (checkBox2.Checked == true)
+                {
+                    photo = "Выбрано";
+                    result *= 1.26;
+                }
+                else
+                {
+                    photo = "Не выбрано";
+                }
+
+                result = Math.Round(result, 2);
+                listBox1.Items.Add(result);
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("ТЫ ЕБАНИНА ТУПАЯ ИДИ НАХУЙ" + ex);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CreateReceipt();
+        }
+
+        public void CreateReceipt()
+        {
+            string date = DateTime.Now.ToString();
+            int number;
+            Random rnd = new Random();
+            number = rnd.Next(1, 9);
+
+            // Путь к вашему шаблону
+            string templatePath = @"C:\Users\Роман\Desktop\123\TESTPROJ\Квитанции\Квитанция-шаблон.docx";
+
+            if (!File.Exists(templatePath))
+            {
+                MessageBox.Show("Файл шаблона не найден");
+                return;
+            }
+
+            Word.Application wordApp = new Word.Application();
+            Word.Document doc = wordApp.Documents.Open(templatePath);
+            // Создаем новый файл с уникальным именем
+            string newFileName = $"Квитанция_{DateTime.Now.ToString("yyyyMMddHHmmss")}.docx";
+            string newFilePath = Path.Combine(Path.GetDirectoryName(templatePath), newFileName);
+
+            ReplaceText(doc, "[number]", number.ToString());
+            ReplaceText(doc, "[height]", textBox1.Text.ToString());
+            ReplaceText(doc, "[weight]", textBox2.Text.ToString());
+            ReplaceText(doc, "[result]", result.ToString());
+            ReplaceText(doc, "[lvl]", lvl);
+            ReplaceText(doc, "[photo]", photo);
+            ReplaceText(doc, "[date]", date);
+            // Сохраняем как новый файл
+            doc.SaveAs(newFilePath);
+            doc.Close();
+
+            wordApp.Visible = true;
+        }
+
+
+        private void ReplaceText(Word.Document doc, string findText, string replaceText)
+        {
+            Word.Range range = doc.Content;
+            range.Find.ClearFormatting();
+            range.Find.Execute(FindText: findText, ReplaceWith: replaceText);
+        }
+    }
+}
